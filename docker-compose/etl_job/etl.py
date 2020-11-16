f5e090bc087dce5b9f29e96f8bdc492e5ca2e189
Lorena Ciutacu <38855851+lorenanda@users.noreply.github.com>
2020-11-16 19:42:30 +0100

    add etl functions

--- docker-compose/etl_job/etl.py
+++ docker-compose/etl_job/etl.py
@@ -4,8 +4,9 @@ from sqlalchemy import create_engine
 import time
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
-import config
-import tweet_streamer
+import logging
+#import config
+#import tweet_streamer
 
 time.sleep(5)
 
@@ -23,12 +24,21 @@ blob_score NUMERIC
 engine.execute(create_query)
 
 # connect to MongoDB 
-client = config.LOCAL_CLIENT
+client = pymongo.MongoClient(host='mongodb', port=27017)
 db = client.tweets_db
 
+
 # Extract tweets from MongoDB
-find_tweets  = db.tweets_collection.find({})
-for tweet in find_tweets:
+def extract_tweets():
+    tweets  = list(db.onthisday.find())
+    if tweets:
+        t = random.choice(tweets)
+        logging.critical("Random tweet: "+t["text"])
+        return t
+
+
+# Sentiment analysis
+def transform_tweets(tweet):
     #clean text
     tweet_text = tweet['text'].replace("\'","")
 
@@ -36,13 +46,24 @@ for tweet in find_tweets:
     sia = SentimentIntensityAnalyzer()
     tweet_sia = sia.polarity_scores(tweet_text)['compound']
     tweet_blob = TextBlob(tweet_text).sentiment
+    return tweet_sia
 
-    # Print out the extracted tweet
-    logging.critical(f'Tweet extracted: {tweet_text}')
 
-    ## Load the data into Postgres
+# Load the data into Postgres
+def load_tweets(tweet, sentiment):
     insert_query = """
-    INSERT INTO tweets (text, sia_score, blob_score)
-    VALUES ('{tweet_text}, {tweet_sia}, {tweet_blob});
+    INSERT INTO tweets VALUES ('{tweet["text"]}', {tweet_sia});
     """
     engine.execute(insert_query)
+    # Print out the extracted tweet
+    logging.critical(f'Tweet {tweet["text"]} loaded into Postgres.')
+
+
+
+logging.critical("Starting ETL job")
+while True:
+    tweet = extract_tweets()
+    if tweet:
+        sentiment = transform_tweets(tweet)
+        load_tweets(tweet, sentiment)
+    time.sleep(10)
