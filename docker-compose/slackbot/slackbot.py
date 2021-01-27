34755be54a3d23f49f4efb645edb33bc7407f26b
lorenanda <lorena.ciutacu@gmail.com>
2021-01-27 16:41:02 +0100

    update airflow

--- docker-compose/slackbot/slackbot.py
+++ docker-compose/slackbot/slackbot.py
@@ -1,9 +1,7 @@
-import time 
+import time
 import slack
 from sqlalchemy import create_engine
 import config
-
-#client = slack.WebClient(token=)
 
 engine = config.PG_ENGINE
 webhook_url = config.WEBHOOK_SLACK
@@ -11,11 +9,12 @@ webhook_url = config.WEBHOOK_SLACK
 
 while True:
     logging.critical("\n\nPositive tweet:\n")
-    query = pg.execute("SELECT text FROM tweets ORDER BY sentiment DESC LIMIT 1")
+    query = pg.execute(
+        "SELECT text FROM tweets ORDER BY sentiment DESC LIMIT 1")
     msg = str(list(query))
     logging.critical(msg + "\n")
     output = f'NEW TWEET! {user} just tweeted: {msg} \nSentiment score: {blob_score}'
-    data = {'text':output}
+    data = {'text': output}
     requests.post(url=webhook_url, json=data)
 
     time.sleep(30)
